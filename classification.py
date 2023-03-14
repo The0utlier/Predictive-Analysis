f86fff4f51b9dc44e2726251055ae8978150b49f
The0utlier <119112033+The0utlier@users.noreply.github.com>
2023-03-14 15:33:41 +0000

    Update classification.py

--- classification.py
+++ classification.py
@@ -57,7 +57,7 @@ X = df[independant]
 
 # Split the data into training and testing sets
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 36)
 
 # Define the model and hyperparameters to tune
 model = RandomForestRegressor()
