ec0d542fbdba144d64324573ee0ab8e8dc50ed23
The0utlier <119112033+The0utlier@users.noreply.github.com>
2023-03-15 19:46:47 +0000

    Update classification.py

--- classification.py
+++ classification.py
@@ -1,21 +1,18 @@
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import SGDClassifier
 import yfinance as yf
 from sklearn.metrics import accuracy_score
 import numpy as np
 import pandas as pd
+from sklearn import svm
 import pandas_ta as ta
 import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
 from sklearn.metrics import r2_score
-from pytrends.request import TrendReq
-# set the seed to 42
 
-df = yf.download(tickers='BTC-USD', period='90d', interval='1d')#cv
+df = yf.download(tickers='BTC-USD', period='10d', interval='90m')#cv
 
+'''
 
-#df['MACD'], df['MACD_Signal'], df['MACD_Hist'] = ta.macd(df.Close)
 df['EMA_12'] = df['Adj Close'].ewm(span=12).mean()
 df['EMA_26'] = df['Adj Close'].ewm(span=26).mean()
 df['Divergence'] = df['EMA_12'] - df['EMA_26']
@@ -38,10 +35,8 @@ df['Fib_0.382'] = (df['High'] - df['Low']) * 0.382 + df['Low']
 df['Fib_0.5'] = (df['High'] - df['Low']) * 0.5 + df['Low']
 df['Fib_0.618'] = (df['High'] - df['Low']) * 0.618 + df['Low']
 df['Fib_0.786'] = (df['High'] - df['Low']) * 0.786 + df['Low']
-
-
+'''
 df['TargetNextClose'] = df['Adj Close'].shift(-1)
-
 
 df['Target'] = np.where(df['TargetNextClose'] > df['Close'], 1, 0)
 
@@ -49,31 +44,87 @@ df = df.dropna()
 
 y = df['Target']
 
-df = df.drop(['Close', 'Adj Close', 'TargetNextClose', 'Target'], axis=1)
+df = df.drop(['Adj Close', 'TargetNextClose', 'Target'], axis=1)
 
-independant = [i for i in df.columns]
-
-X = df[independant]
+X = df
 
 # Split the data into training and testing sets
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 36)
 
-# Define the model and hyperparameters to tune
-model = RandomForestRegressor()
-params = {'n_estimators': [10, 50, 100],
-          'max_depth': [5, 10, 20]}
+rfc = RandomForestClassifier(n_estimators=100, random_state=42)
 
-# Perform grid search for hyperparameter tuning
-grid_search = GridSearchCV(model, params, cv=5)
-grid_search.fit(X_train, y_train)
+# Fit the model to the training data
+rfc.fit(X_train, y_train)
 
-# Predict the target variable for the testing set using the trained model
-y_pred = grid_search.predict(X_test)
+# Predict the values of the test set
+y_pred = rfc.predict(X_test)
 
 # Convert the predicted values to binary using a threshold of 0.5
-y_pred_binary = np.where(y_pred > 0.5, 1, 0)
+print(len(y_pred))
+
+print(y_pred)
+
 
 # Evaluate the accuracy of the predictions
-accuracy = accuracy_score(y_test, y_pred_binary)
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+
+svc = svm.SVC() #RBF
+
+# Fit the model to the training data
+svc.fit(X_train, y_train)
+
+# Predict the values of the test set
+y_pred = svc.predict(X_test)
+
+print(y_pred)
+
+# Convert the predicted values to binary using a threshold of 0.5
+
+# Evaluate the accuracy of the predictions
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+
+clf = SGDClassifier(loss="hinge", penalty="l2", max_iter=2000)
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+
+print(y_pred)
+
+# Convert the predicted values to binary using a threshold of 0.5
+
+# Evaluate the accuracy of the predictions
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+print(y_pred)
+
+# Convert the predicted values to binary using a threshold of 0.5
+
+# Evaluate the accuracy of the predictions
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+
+
+from sklearn.neural_network import MLPClassifier
+
+clf = MLPClassifier(solver='adam', alpha=1e-5, max_iter=10000,
+                     hidden_layer_sizes=(10,5,2), random_state=1)
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+print(y_pred)
+
+# Convert the predicted values to binary using a threshold of 0.5
+
+# Evaluate the accuracy of the predictions
+accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy:", accuracy)
