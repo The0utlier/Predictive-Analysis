a9b77f9caacbab2c3d73cabf42c3165ede9bff55
The0utlier <119112033+The0utlier@users.noreply.github.com>
2023-03-15 20:48:00 +0000

    Update randomforest.py

--- randomforest.py
+++ randomforest.py
@@ -1,5 +1,5 @@
-
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 import yfinance as yf
 from sklearn.metrics import accuracy_score
@@ -11,11 +11,14 @@ import seaborn as sns
 import numpy as np
 from sklearn.metrics import r2_score
 from sklearn.metrics import mean_absolute_error
-# set the seed to 42
+from sklearn.preprocessing import StandardScaler
 
-df = yf.download(tickers='BTC-USD', period='10d', interval='30m')#cv
 
+
+df = yf.download(tickers='BTC-USD', period='10d', interval='30m')
 df['TargetNextClose'] = df['Adj Close'].shift(-1)
+
+'''
 df['EMA_12'] = df['Adj Close'].ewm(span=12).mean()
 df['EMA_26'] = df['Adj Close'].ewm(span=26).mean()
 df['Divergence'] = df['EMA_12'] - df['EMA_26']
@@ -38,40 +41,40 @@ df['Fib_0.382'] = (df['High'] - df['Low']) * 0.382 + df['Low']
 df['Fib_0.5'] = (df['High'] - df['Low']) * 0.5 + df['Low']
 df['Fib_0.618'] = (df['High'] - df['Low']) * 0.618 + df['Low']
 df['Fib_0.786'] = (df['High'] - df['Low']) * 0.786 + df['Low']
+'''
 
-columns = [i for i in df.columns if i != "Close"]
-print(columns)
-df = df[columns]
-df = df.dropna()
+df = df.drop(columns=['Adj Close'])
+df.dropna(inplace=True) #false
+# Remove outliers using the IQR method
+Q1 = df.quantile(0.25)
+Q3 = df.quantile(0.75)
+IQR = Q3 - Q1
+df = df[~((df < (Q1 - 1.5 * IQR)) |(df > (Q3 + 1.5 * IQR))).any(axis=1)]
 
-
-
-independant = [i for i in columns if i != 'TargetNextClose']
+independant = [i for i in df.columns if i != 'TargetNextClose']
 X = df[independant]
 y = df['TargetNextClose']
-#X = X[corr[corr[0] > 0.05].index]#0.75
-from sklearn.model_selection import train_test_split
 
-# Split the data into training and testing sets
+# Normalize the data using StandardScaler
+scaler = StandardScaler()
+df[independant] = scaler.fit_transform(df[independant])
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-#perfect = cross_validation()['n_estimators']
-#print(perfect)
-#np.random.seed(154)#randint
-# Instantiate the model with 100 decision trees
-
-rf = RandomForestRegressor(n_estimators=500)#400)
+rf = RandomForestRegressor(n_estimators=500)
 
 rf.fit(X_train, y_train)
 
-# Make predictions on the test data
-#y_pred = rf.predict(X[-1:])
-
-#print(f"Prediction for tomorrow: {y_pred}")
-
 y_pred = rf.predict(X_test)
 
-print(f"Tomorrow's prediction {y_pred[-1]}")
-
 mae = mean_absolute_error(y_test, y_pred)
-print(mae)
+print(mae)# create a new dataframe for test data
+
+y_test_targets = rf.predict(X_test)
+# create a new dataframe for test data
+
+test_df = pd.concat([X_test, y_test], axis=1)
+
+test_df['Direction'] = np.where(test_df['TargetNextClose'] > test_df['Close'], 1, 0)
+
+
