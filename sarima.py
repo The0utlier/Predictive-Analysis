754bb6417e52051e9bcb4a211892dd3f38873b9d
The0utlier <119112033+The0utlier@users.noreply.github.com>
2023-03-28 00:10:42 +0100

    Update sarima.py

--- sarima.py
+++ sarima.py
@@ -10,59 +10,45 @@ warnings.filterwarnings('ignore')
 
 # Download data and create dataframe
 df = yf.download(tickers='BTC-USD', period='120d', interval='1d')
-data = df.iloc[:-7]
+data = df.iloc[:-3]
 
-# Define parameter grid
-p = range(0, 5)
-d = range(0, 5)
-q = range(0, 5)
+# Define the parameter grid
+p = range(0, 3)
+d = range(1, 3)
+q = range(0, 3)
 P = range(0, 3)
-D = range(0, 3)
+D = range(1, 3)
 Q = range(0, 3)
 s = 7
-pdq = list(product(p, d, q))
-seasonal_pdq = list(product(P, D, Q, [s]))
+param_grid = product(p, d, q, P, D, Q)
 
-# Fit and evaluate models for each parameter combination
-mae_dict = {}
-for param in pdq:
-    for param_seasonal in seasonal_pdq:
-        try:
-            model = SARIMAX(data['Close'], order=param, seasonal_order=param_seasonal)
-            model_fit = model.fit()
-            forecast = model_fit.forecast(steps=7)
-            actual = df['Close'][-7:]
-            mae = mean_absolute_error(actual, forecast)
-            mae_dict[(param, param_seasonal)] = mae
-            print('SARIMA{}x{} - MAE: {}'.format(param, param_seasonal, mae))
-        except:
-            continue
+# Perform grid search
+best_mae = np.inf
+best_params = None
+for params in param_grid:
+    try:
+        model = SARIMAX(data['Close'], order=(params[0], params[1], params[2]), 
+                        seasonal_order=(params[3], params[4], params[5], s))
+        model_fit = model.fit()
+        forecast = model_fit.forecast(steps=3)
+        actual = df['Close'][-3:]
+        mae = mean_absolute_error(actual, forecast)
+        if mae < best_mae:
+            best_mae = mae
+            best_params = params
+    except:
+        continue
 
-# Find parameter combination with lowest MAE
-best_params = min(mae_dict, key=mae_dict.get)
-print('Best parameters: ', best_params)
+# Print the best parameters and MAE
+print("Best parameters:", best_params)
+print("Best MAE:", best_mae)
 
-# Split data
-train_data = df.iloc[:-7]
-test_data = df.iloc[-7:]
-
-model = SARIMAX(data['Close'], order=best_params[0], seasonal_order=best_params[1])
+# Plot results with the best model
+model = SARIMAX(data['Close'], order=(best_params[0], best_params[1], best_params[2]), 
+                seasonal_order=(best_params[3], best_params[4], best_params[5], s))
 model_fit = model.fit()
-
-# forecast next 7 days
-forecast = model_fit.forecast(steps=7)
-
-actual = df['Close'][-7:]
-
-# calculate MAE
-mae = mean_absolute_error(actual, forecast)
-
-print("MAE:", mae)
-
-# Plot results
-import matplotlib.pyplot as plt
-
-plt.plot(df.index[-10:], df['Close'][-10:], label='Actual')
-plt.plot(pd.date_range(start=df.index[-8], periods=8, freq='D'), forecast, label='Predicted')
+forecast = model_fit.forecast(steps=3)
+plt.plot(df.index[-100:], df['Close'][-100:], label='Actual')
+plt.plot(forecast, label='Forecast')
 plt.legend()
 plt.show()
